2b579cbb784d941767a8a1814835f967fb08b473
Gutierrez Muriedas, Juan Pedro <juanpedroghm@gmail.com>
2018-12-08 13:10:11 +0100

    Shit aint working yet

--- tests/DM-STDP/rm-stdp.py
+++ tests/DM-STDP/rm-stdp.py
@@ -5,22 +5,33 @@ from pyNN.utility.plotting import Figure, Panel
 import numpy as np
 import matplotlib.pyplot as plt
 import gym
+from functools import reduce
+import quantities as units
 
-# %% Constants and config params
+## Configuration params
 inputNeuronFactor = 20
-outputNeuronFactor = 20
+outputNeuronFactor = 10
 env_name = 'MountainCar-v0'
-step_size = 50.0
-render = True
-trials = 100
+step_size = 10.0
+render = False
+trials = 200
 
-# %% Helper Functions
+## Training params 
+Apos = 2
+Aneg = -2
+tauPos = 40
+tauNeg = 40
+tauTrace = 80
+
+learningRate = 1
+
+## Helper functions and classes
 def getAction():
     pass
 
 def getRate(value, minV, maxV):
     minRate = 0.0
-    maxRate = 1/(step_size /1000)
+    maxRate = 50.0
     return (value - minV)/(maxV - minV) * (maxRate - minRate) + minRate
 
 def plot_spiketrains(inputPop, outputPop, filename):
@@ -31,7 +42,65 @@ def plot_spiketrains(inputPop, outputPop, filename):
         title="Helper plots"
     ).save(normalized_filename(filename, "spikes", "png", options.simulator))
 
-# %% setup env
+
+class SpikeTraceTracker():
+
+    def __init__(self, shape, inputPop, outputPop, sampling_rate = 1.0):
+        self.shape = shape
+        self.sampling_rate = sampling_rate
+        self.inputPop = inputPop
+        self.outputPop = outputPop
+        self.segment = 0
+        self.reset()
+    
+    def __call__(self, t):
+
+        deltaTrace = np.zeros(self.shape)
+        inputSpikes, outputSpikes = [], []
+        try:
+            inputSpikes = self.inputPop.get_data().segments[0].spiketrains
+            outputSpikes = self.outputPop.get_data().segments[0].spiketrains
+        except:
+            return t + self.sampling_rate
+        
+        for j in range(len(inputSpikes)):
+            if self.neuronFired(inputSpikes[j], t):
+                self.Pplus[j] += Apos
+                deltaTrace[j,:] += self.Pminus
+            else:
+                self.Pplus[j] *= np.exp(-self.sampling_rate / tauPos)
+
+        for i in range(len(outputSpikes)):
+            if self.neuronFired(outputSpikes[i], t):
+                self.Pminus[i] += Aneg
+                deltaTrace[:,i] += self.Pplus
+                self.spikeCount[i] += 1
+            else:
+                self.Pminus[i] *= np.exp(-self.sampling_rate / tauNeg)
+            
+        self.eTrace = self.eTrace * np.exp(-self.sampling_rate / tauTrace) + deltaTrace
+
+        return t + self.sampling_rate
+    
+    def neuronFired(self, neuronSpiketrain, t):
+        if len(neuronSpiketrain) > 0:
+            return neuronSpiketrain[-1] >= (t - self.sampling_rate)
+        else:
+            return False
+    
+    def reset(self):
+        self.Pplus = np.zeros((self.shape[0], ))
+        self.Pminus = np.zeros((self.shape[1], ))
+        self.eTrace = np.zeros(self.shape)
+        self.spikeCount = np.zeros((self.shape[1],))
+    
+    def resetCount(self):
+        self.spikeCount = np.zeros((self.shape[1],))
+    
+    def setSegmentIndex(self, index):
+        self.segment = index
+
+##
 env = gym.make(env_name)
 env.reset()
 
@@ -42,7 +111,7 @@ nObservations = env.observation_space.shape[0]
 sim, options = get_simulator(("--plot-figure", "Plot the simulation results to a file.",
                               {"action": "store_true"}))
 
-sim.setup()
+sim.setup(timestep = 1.0)
 
 params = {
     'a' : 0.02,
@@ -53,27 +122,35 @@ params = {
 
 inputPop = sim.Population(nObservations * inputNeuronFactor, sim.SpikeSourcePoisson())
 outputPop = sim.Population(nActions * outputNeuronFactor, sim.Izhikevich(**params))
-syn = sim.StaticSynapse(weight=rnd('uniform', [0, 5]))
+syn = sim.StaticSynapse(weight=rnd('uniform', [0, 3]))
 connections = sim.Projection(inputPop, outputPop, sim.AllToAllConnector(), syn)
+
+weights = connections.get('weight', format='array')
 
 inputPop.record('spikes')
 outputPop.record(['spikes', 'v'])
 
+
+spikeTraceTracker = SpikeTraceTracker(weights.shape, inputPop, outputPop)
+
 for i in range(trials):
 
-    done = True
-    observation = []
-    totalReward = 0
+    done = False
+    observation = env.reset()
+    totalReward = 0.0
+    totalAdjustedReward = 0.0
+    spikeTraceTracker.setSegmentIndex(i)
+    lastSpeed = 0.0
 
-    while(True):
+    maxSpeed = 0.0
 
-        if done:
-            obsevation = env.reset()
+    while(not done):
 
         if render:
             env.render()
 
-            ## feed env output to net
+        ## feed env output to net
+
         for value, minV, maxV, index in zip(observation,
                                         env.observation_space.low,
                                         env.observation_space.high,
@@ -81,16 +158,44 @@ for i in range(trials):
             inputPop[index*inputNeuronFactor:(index+1)*inputNeuronFactor].set(rate=getRate(value, minV, maxV ))
 
         # Rum network simulation
-        sim.run(step_size)
+        sim.run(step_size, callbacks=[spikeTraceTracker])
 
-        ## read net output for action
-        action = env.action_space.sample()
+        ## read net output for action 
+        # Divide the neurons equally between all the actions, the group of neurons that fire the most
+        # in the last time step, is the action
+
+        # There must be a better way to do this 
+        actionCount = [x.sum() for x in np.split(spikeTraceTracker.spikeCount, nActions)]
+        spikeTraceTracker.resetCount()
+
+        action = 0 
+        if sum(actionCount) > 0:
+            p = np.array(actionCount) / sum(actionCount)
+            action = np.random.choice(nActions, p = p)
+        else:
+            action = np.random.choice(nActions)
         
         ## take env step
         observation, reward, done, info = env.step(action)
         totalReward += reward
+        
+        reward = reward if reward > 0 else 0
+        reward += np.abs(observation[1]) - lastSpeed
+        totalAdjustedReward += reward
+
+        lastSpeed = np.abs(observation[1]) 
+        maxSpeed = np.max([lastSpeed, maxSpeed])
+        # print(f'Action Count: {actionCount}, Action #: {action}, Reward: {reward}, Speed: {lastSpeed}' )
 
         ## perform learning step
-        
+        weights += learningRate * reward * spikeTraceTracker.eTrace
+        connections.set(weight = weights)
 
+    
+    ## Finished simulation
+    print(f'Trial {i}, Total env reward = {totalReward}, Total adjusted reward: {totalAdjustedReward}, Max speed: {maxSpeed}' )
+        
 # plot_spiketrains(inputPop, outputPop, 'results')
+sim.end()
+print('Simulation ended')
+
